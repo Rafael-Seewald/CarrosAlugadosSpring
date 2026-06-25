5ccb82226cb7a7b0833fcf7a72a567af9984b9cf
Rafael-Seewald <rafa.vini.seewald@gmail.com>
2026-06-24 21:49:41 -0300

    Primeiro commit arquivos base

---
+++ src/main/java/seewald/rafael/cadastroCarrosAlugados/CadastroCarrosAlugadosApplication.java
@@ -0,0 +1,13 @@
+package seewald.rafael.cadastroCarrosAlugados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCarrosAlugadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCarrosAlugadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seewald/rafael/cadastroCarrosAlugados/CadastroCarrosAlugadosApplicationTests.java
@@ -0,0 +1,13 @@
+package seewald.rafael.cadastroCarrosAlugados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCarrosAlugadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
